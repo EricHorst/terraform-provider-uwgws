62edef23462fefc11c04bb97cbc89c485e0fa458
Eric Horst <erich@uw.edu>
2020-01-23 20:27:23 -0800

    update to sdk example

--- provider.go
+++ provider.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap: map[string]*schema.Resource{
-			"example_server": resourceServer(),
-		},
+		ResourcesMap: map[string]*schema.Resource{},
 	}
 }
